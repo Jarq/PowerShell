b75a85b39a3194fd4c9348f19d7b0617db60592f
Jarq <Jarq@users.noreply.github.com>
2018-11-06 10:45:12 +0100

    Update disable_enable_user_acc.ps1
    place 'users.txt' in your desktop with all affected user accounts. With the menu you can choose what to do, either disable or enable multiple user accounts

---
+++ disable_enable_user_acc.ps1
@@ -0,0 +1,31 @@
+﻿$module=(Get-Module).Name
+if ($module -notmatch 'ActiveDirectory') {
+Import-Module ActiveDirectory
+}
+
+$menu= read-host -Prompt "
+`n1. Disable Accounts
+`n2. Enable Accounts
+`n3. Exit
+`nPlease Select a number"
+switch($menu) {
+    1{
+    $whoami=$env:username
+    $File=get-content "C:\Users\$whoami\Desktop\users.txt"
+    foreach ($user in $File) {
+    disable-ADAccount $user
+    }}
+    
+    2{
+    $whoami=$env:username
+    $File=get-content "C:\Users\$whoami\Desktop\users.txt"
+    foreach ($user in $File) {
+    enable-ADAccount $user
+    }}
+    3{
+    exit
+    }}
+    
+
+
+
