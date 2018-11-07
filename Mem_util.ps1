1994d975c57a6fc96be7a783a74bd1d4d2c6d514
Jarq <Jarq@users.noreply.github.com>
2018-11-07 10:46:34 +0100

    Update Mem_util.ps1
    Gives memory utilization as an output.

---
+++ Mem_util.ps1
@@ -0,0 +1,7 @@
+﻿$TotalMem=[math]::Round((Get-WmiObject -Class Win32_ComputerSystem ).TotalPhysicalMemory /1MB)
+$FreeMem=[math]::Round((Get-WmiObject -Class Win32_operatingsystem ).FreePhysicalMemory /1kb)
+ 
+$result=$TotalMem - $FreeMem
+$hname=hostname
+ 
+Write-Output = "Memory usage on $hname is $result MB "   
