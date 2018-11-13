41c6af6f80d013675e25230aa48b5a81092bec6d
Jarq <Jarq@users.noreply.github.com>
2018-11-13 11:25:48 +0100

    Update iisconfig.ps1

--- iisconfig.ps1
+++ iisconfig.ps1
@@ -11,9 +11,7 @@ Import-Module WebAdministration
 
 $defweb=Read-Host "Please Specify your Default web Site name: "
 $var=Get-WebConfigurationProperty -filter /system.web/httpRuntime -name ExecutionTimeout -PSPath "IIS:\Sites\Default Web Site\$defweb" | select Name, Value
-
 $var1=Get-WebConfigurationProperty -filter /system.web/httpRuntime -name MaxRequestLength -PSPath "IIS:\Sites\Default Web Site\$defweb" | select Name, Value
-
 #$var 
 #$var1
 
